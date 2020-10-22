48cc7a3f52593316e1458893a4893c25b3a3a95d
Christophe Grand <christophe@cgrand.net>
2020-10-22 14:47:56 +0200

    removing non reader stuff

--- lib/reader.dart
+++ lib/reader.dart
@@ -222,41 +222,6 @@ final DOUBLE_REGEXP = RegExp(r"([-+]?[0-9]+(\.[0-9]*)?([eE][-+]?[0-9]+)?)(M)?$")
 // differs from Clojure: :3a and a/:x are not valid
 final SYMBOL_REGEXP = RegExp(r"([:]{1,2})?(?:([^0-9/:].*)/)?(/|[^0-9/:][^/]*)$");
 
-int hashCombine(int seed, int hash) {
-  seed ^= hash + 0x9e3779b9 + (seed << 6) + (seed >> 2);
-  return seed;
-}
-
-class Symbol {
-  final String namespace;
-  final String name;
-  const Symbol._internal(this.namespace, this.name);
-  const factory Symbol(String namespace, String name) = Symbol._internal;
-  String toString() { return namespace != null ? "$namespace/$name" : "$name"; }
-  bool operator == (other) => other is Symbol && namespace == other.namespace && name == other.name;
-  int get hashCode {
-    var h = 775073750;
-    hashCombine(h, namespace.hashCode);
-    hashCombine(h, name.hashCode);
-    return h;
-  }
-}
-
-class Keyword {
-  final String namespace;
-  final String name;
-  const Keyword._internal(this.namespace, this.name);
-  const factory Keyword(String namespace, String name) = Keyword._internal;
-  String toString() { return namespace != null ? ":$namespace/$name" : ":$name"; }
-  bool operator == (other) => other is Keyword && namespace == other.namespace && name == other.name;
-  int get hashCode {
-    var h = 2030774975;
-    hashCombine(h, namespace.hashCode);
-    hashCombine(h, name.hashCode);
-    return h;
-  }
-}
-
 dynamic interpretToken(String token) {
   switch (token) {
     case 'nil': return null;
