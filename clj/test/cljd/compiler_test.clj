1d74208475274f0a28b8a0cebc6f9657c9e606b3
Baptiste Dupuch <baptiste.dupuch@gmail.com>
2021-07-29 16:22:36 +0200

    Updates license to EPLv1

--- clj/test/cljd/compiler_test.clj
+++ clj/test/cljd/compiler_test.clj
@@ -1,3 +1,14 @@
+;   Copyright (c) Baptiste Dupuch & Christophe Grand . All rights reserved.
+;   The use and distribution terms for this software are covered by the
+;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;   which can be found in the file epl-v10.html at the root of this distribution.
+;   By using this software in any fashion, you are agreeing to be bound by
+;   the terms of this license.
+;   You must not remove this notice, or any other, from this software.
+
+; @WARNING: Thu Jul 29 16:19:43 2021 : Tests are outdated but we keep
+; them because we plan to fix them.
+
 (ns cljd.compiler-test
   (:require
     [clojure.test :refer :all]
