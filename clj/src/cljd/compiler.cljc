Tensegritics/ClojureDart

commit 85bb834ebc57eceb40d15c1c94e66017f230bb5c

simplify emit-quote(d) (less allocs)

clj/src/cljd/compiler.cljc
@@ -784,10 +784,10 @@
       (seq bindings) (list 'dart/let bindings))))
 
 (defn emit-coll
-  ([coll env] (emit-coll identity coll env))
-  ([f coll env]
+  ([coll env] (emit-coll emit coll env))
+  ([emit coll env]
    (if (seq coll)
-     (let [items (into [] (comp (if (map? coll) cat identity) (map f)) coll)
+     (let [items (into [] (if (map? coll) cat identity) coll)
            [bindings items]
            (reduce (fn [[bindings fn-call] x]
                      (let [[bindings' x'] (lift-arg (seq bindings) (emit x env) "item" env)]
@@ -1592,11 +1592,14 @@
       (emit (list 'quote (symbol (name (:ns info)) (name (:name info)))) {})
       (throw (Exception. (str "Not a var: " s (source-info)))))))
 
-(defn emit-quoted [[_ x] env]
+(defn emit-quoted [x env]
   (cond
-    (coll? x) (emit-coll #(list 'quote %) x env)
+    (coll? x) (emit-coll emit-quoted x env)
     (symbol? x) (emit (list 'cljd.core/symbol (namespace x) (name x)) env)
     :else (emit x env)))
+
+(defn emit-quote [[_ x] env]
+  (emit-quoted x env))
 
 (defn ns-to-lib [ns-name]
   (str *target-subdir* (replace-all (name ns-name) #"[.]" {"." "/"}) ".dart"))
@@ -1747,7 +1750,7 @@
                        ns emit-ns
                        try emit-try
                        case* emit-case*
-                       quote emit-quoted
+                       quote emit-quote
                        do emit-do
                        var emit-var
                        let* emit-let*

commit 5f8d890c08d320b39d837358cfa7bd8748f8fa82

fix emit-quoted bug introduced in 85bb834ebc57eceb40d15c1c94e66017f230bb5c

clj/src/cljd/compiler.cljc
@@ -805,12 +805,12 @@
 
 (defn emit-coll
   ([coll env] (emit-coll emit coll env))
-  ([emit coll env]
+  ([maybe-quoted-emit coll env]
    (if (seq coll)
      (let [items (into [] (if (map? coll) cat identity) coll)
            [bindings items]
            (reduce (fn [[bindings fn-call] x]
-                     (let [[bindings' x'] (lift-arg (seq bindings) (emit x env) "item" env)]
+                     (let [[bindings' x'] (lift-arg (seq bindings) (maybe-quoted-emit x env) "item" env)]
                        [(concat bindings' bindings) (cons x' fn-call)]))
              [nil ()] (rseq items))
            fn-sym (cond