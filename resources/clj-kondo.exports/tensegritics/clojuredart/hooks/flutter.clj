31818828c9250400ac17273ea8fcfea9d00d00a6
liverm0r <arturdumchev@yandex.ru>
2022-04-26 00:40:13 +0300

    fix widget hook, use custom linter

--- resources/clj-kondo.exports/tensegritics/clojuredart/hooks/flutter.clj
+++ resources/clj-kondo.exports/tensegritics/clojuredart/hooks/flutter.clj
@@ -1,81 +1,107 @@
 (ns hooks.flutter
-  (:require [clj-kondo.hooks-api :as api]
+  (:require [clj-kondo.hooks-api :as api :refer [sexpr token-node list-node]]
             [clojure.set :as set]
             [clojure.string :as str]))
 
 (defn- with->bindings
-  "Get :with form, returns mapcat-ed bindigs"
-  [with]
+  "Returns :with bindings vector, :let included"
+  [with-node]
   (into []
         (mapcat (fn [[lhs rhs :as binding]]
                   (condp = (str lhs)
                     ":dispose" nil
                     ":let" (:children rhs)
                     binding)))
-        (partition 2 (:children with))))
+        (partition 2 (:children with-node))))
+
+(defn- bind-identity 
+  "Returns node-binding like [symbol identity] or nil"
+  [symb]
+  (when symb [(token-node symb) (api/token-node 'identity)]))
+
+(defn- with->binding+dispose 
+  "Returns seq with sequences like ([name value :duspose form] ...)"
+  [with]
+  (filter #(-> % (nth 2) (= :dispose)) (partition 4 1 (list* nil nil with))))
+
+(defn- find! [condition node msg]
+  (when condition 
+    (api/reg-finding! (assoc (meta node) :message msg :type :cljd/widget))))
 
 (defn widget 
   "Hook for cljd.flutter.alpha/widget macro"
   [{:keys [:node]}]
-    (let [args (rest (:children node))
-          opts-nodes (take-while (comp api/keyword-node? first) (partition 2 args))
-          pairs->value (fn [k] (first (filter #(-> % first (api/sexpr) (= k)) opts-nodes)))
-          [_ state] (pairs->value :state)
-          [s-name s-value] (:children state)
-          [_ context] (pairs->value :context)
-          [_ watch] (pairs->value :watch)
-          [_ key] (pairs->value :key)
-          [_ ticker] (pairs->value :ticker)
-          [_ tickers] (pairs->value :tickers)
-          [_ with] (pairs->value :with)
-          bindings-count (* 2 (count opts-nodes))
-          body (drop bindings-count args)
-          error (fn [msg] (throw (ex-info msg {})))
-          unknown-keys (set/difference 
-                         (into #{} (map (comp api/sexpr first) opts-nodes))
-                         #{:state :key :watch :context :with :ticker :tickers})]
-      
-      (cond
-        (and (seq (filter api/keyword-node? args))   ; has top keywords
-             (not (api/keyword-node? (first args)))) ; but first form is not a keyword
-        (error (str "Unknown first form " (api/sexpr (first args))))
+  (let [args (rest (:children node))
+        opts-nodes (take-while (comp api/keyword-node? first) (partition 2 args))
+        pairs->value (fn [k] (first (filter #(-> % first (api/sexpr) (= k)) opts-nodes)))
+        [_ with-node] (pairs->value :with)
 
-        (and state watch)
-        (error ":state and :watch option keys are mutually exclusive")
+        {:keys [state context ticker tickers watch key with] :as opts} 
+        (into {} (map (fn [[k v]] (vector (sexpr k) (sexpr v)))) opts-nodes)
 
-        (and ticker tickers)
-        (error "Both :ticker and :tickers are not allowed")
+        bindings-count (* 2 (count opts-nodes))
+        body (drop bindings-count args)
 
-        (and (not (or state watch)) (or with ticker tickers))
-        (error "keys [:tickers :ticker :with] are only allowed with either :state or :watch")
-        
-        (and with 
-             (or (not (api/vector-node? with))
-                 (some #(not (or (api/token-node? %) (api/keyword-node? %))) 
-                       (take-nth 2 (:children with)))))
-        (error ":with left hand simbols should be simple simbols, :let, or :dispose forms")
+        unknown-keys (set/difference 
+                       (into #{} (keys opts))
+                       #{:state :key :watch :context :with :ticker :tickers})]
 
-        (and state (or (not (api/vector-node? state))
-                       (not= (count (:children state)) 2)
-                       (not (api/token-node? s-name))))
-        (error ":state should be a vector [name initial-value]")
+    (find! (not= (count opts-nodes) (count (keys opts)))
+           node
+           "Duplicate keys")
 
-        (seq unknown-keys)
-        (error (str "Unsupported option keys " (str/join " " unknown-keys)))
+    (find! (= 0 (- (count args) bindings-count)) 
+           node
+           (str "No body provided"))
+    
+    (find! (and (seq (filter api/keyword-node? args))   ; has top keywords
+                (not (api/keyword-node? (first args)))) ; but first form is not a keyword
+           (first args)
+           (str "Unknown first form " (api/sexpr (first args))))
 
-        (= 0 (- (count args) bindings-count)) 
-        (error (str "No body provided"))
+    (find! (and state watch)
+           (first (pairs->value :state))
+           ":state and :watch option keys are mutually exclusive")
 
-        :else 
-        {:node (api/list-node 
-                 (list
-                   (api/token-node 'let)
-                   (api/vector-node 
-                     (concat (if context [context (api/token-node 'identity)] []) 
-                             (if ticker [ticker (api/token-node 'identity)] [])
-                             (if tickers [tickers (api/token-node 'identity)] [])
-                             (if state [s-name (api/list-node [(api/token-node 'atom) s-value])] [])
-                             (with->bindings with)))
-                   watch ;; to lint 'watch and 'key  
-                   key
-                   body))})))
+    (find! (and with 
+                (or (not (vector? with))
+                    (some #(not (or (symbol? %) (keyword? %))) (take-nth 2 with))))
+           with-node 
+           ":with left hand simbols should be simple simbols, :let, or :dispose")
+
+    (find! (and ticker tickers)
+           (first (pairs->value :ticker))
+           "Both :ticker and :tickers are not allowed")
+
+    (find! (some
+             (fn [[disposable _ _ dispose-form]]
+               (or (not (symbol? disposable))
+                   (not (or (symbol? dispose-form) (seq? dispose-form)))))
+             (with->binding+dispose with))
+           with-node
+           ":dispose should locate right after disposable pair and be an expression form")
+
+    (find! (and state (or (not (vector? state))
+                          (not= (count state) 2)
+                          (not (symbol? (first state)))))
+           (first (pairs->value :state))
+           ":state should be a vector [name initial-value]")
+
+    (find! (seq unknown-keys)
+           node
+           (str "Unsupported option keys " (str/join " " unknown-keys)))
+
+    {:node (api/list-node 
+               (list*
+                 (token-node 'let)
+                 (api/vector-node 
+                   (concat (bind-identity context) 
+                           (bind-identity ticker)
+                           (bind-identity tickers)
+                           (when (vector? state) ;; TODO when   
+                             [(token-node (first state))
+                              (list-node [(token-node 'atom) (token-node (second state))])])
+                           (with->bindings with-node)))
+                 (when watch (with-meta (token-node watch) (meta (second (pairs->value :watch)))))
+                 (when key (with-meta (token-node key) (meta (second (pairs->value :key)))))
+                 body))}))
