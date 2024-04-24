cedd255692099bb5b068903f7a96c46ff6646901
Erdos Balint <valerauko@gmail.com>
2024-04-24 11:40:36 +0900

    Only exit on error

--- clj/src/cljd/build.clj
+++ clj/src/cljd/build.clj
@@ -316,7 +316,7 @@
                       (println (str "💀 Flutter sub-process exited with " (.exitValue p)))))
                   (finally
                     (some-> p .destroy)))))
-            (System/exit (if compilation-success 0 1))))))))
+            (or compilation-success (System/exit 1))))))))
 
 (defn test-cli [& {:keys [namespaces]}]
   (when (compile-cli :namespaces namespaces)
